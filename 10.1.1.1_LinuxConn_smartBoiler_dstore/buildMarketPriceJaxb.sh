3e18dd3f1f7d189a394bdaf5655e9973e3f35dbc
Marco Descher <marco@descher.at>
2013-08-12 06:56:39 +0000

    Starting marketPriceInformation part

---
+++ 10.1.1.1_LinuxConn_smartBoiler_dstore/buildMarketPriceJaxb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/bin/xjc -p at.fhv.marketprice -d src-gen rsc/MarketPriceInformationList.xsd
---
+++ 10.1.1.1_LinuxConn_smartBoiler_dstore/src/at/fhv/smartgrid/rasbpi/marketprice/MarketPriceHelper.java
@@ -0,0 +1,17 @@
+package at.fhv.smartgrid.rasbpi.marketprice;
+
+import at.fhv.marketprice.MarketPriceInformation;
+
+public class MarketPriceHelper {
+
+	public static String MARKETPRICE_XML_FILE_LOCATION = "";
+	
+	private static MarketPriceInformation mpi = null;
+	
+	public static long getCurrentMarketPricesListTimestamp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
