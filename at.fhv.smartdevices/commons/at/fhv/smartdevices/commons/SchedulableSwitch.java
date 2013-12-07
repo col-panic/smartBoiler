e51e5d29bbd53443e09ccf2b2abd4e47590c2cc8
PeterKepplinger <PeterKepplinger>
2013-12-07 15:58:21 +0100

    schedulable switch!

---
+++ at.fhv.smartdevices/commons/at/fhv/smartdevices/commons/SchedulableSwitch.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package at.fhv.smartdevices.commons;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import at.fhv.smartgrid.rasbpi.ISmartController;
+
+/**
+ * @author kepe_nb
+ *
+ */
+public class SchedulableSwitch implements ISchedulable {
+
+	private ISmartController _controller;
+	private Clock _clock;
+	
+	private SerializableTreeMap<Long, Boolean> _switchingTimes = new SerializableTreeMap<Long,Boolean>();
+	private Long _next;
+	private Boolean _running =false;
+	private Lock _lock = new ReentrantLock();
+	
+	public SchedulableSwitch(ISmartController controller, Clock clock)
+	{
+		_controller = controller;
+		
+		_clock = clock;
+	}
+	public void setSwitchingTimes(SerializableTreeMap<Long, Boolean> switchingTimes)
+	{
+		_lock.lock();
+				
+		_switchingTimes.putAll(switchingTimes);
+		
+		_lock.unlock();		
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {	
+		
+		_lock.lock();
+		_running =true;
+		
+		if(_switchingTimes!=null)
+		{
+			if(_switchingTimes.containsKey(_next))
+			{
+				_controller.setRelaisPowerState(_switchingTimes.get(_next));
+			}
+		}		
+		_running = false;
+		_lock.unlock();
+	}
+
+	/* (non-Javadoc)
+	 * @see at.fhv.smartdevices.commons.ISchedulable#getScheduleTimeStep()
+	 */
+	@Override
+	public long getScheduleTimeStep() {
+		
+		_next = _switchingTimes.ceilingEntry(_clock.getDate()).getKey();
+		
+		return _next - _clock.getDate();
+	}
+
+	/* (non-Javadoc)
+	 * @see at.fhv.smartdevices.commons.ISchedulable#getPriority()
+	 */
+	@Override
+	public int getPriority() {
+		
+		return 10;
+	}
+
+	/* (non-Javadoc)
+	 * @see at.fhv.smartdevices.commons.ISchedulable#isRunning()
+	 */
+	@Override
+	public Boolean isRunning() {
+		
+		return _running;
+	}
+
+}
